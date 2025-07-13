086b26ce3aa16e1334f77feaa6f015ef1229e6d8
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2025-07-13 18:38:35 +0700

    feat: use trunk to build web target
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- src/lib.rs
+++ src/lib.rs
@@ -60,20 +60,20 @@ pub fn parse_ciphertext(ciphertext: &str) -> Result<LaravelEncryptedData, String
 // ----------------------------------------------------------------------------
 // When compiling for web:
 
-#[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::{self, prelude::*};
+// #[cfg(target_arch = "wasm32")]
+// use eframe::wasm_bindgen::{self, prelude::*};
 
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub fn start(canvas_id: &str) -> Result<(), eframe::wasm_bindgen::JsValue> {
-    let web_options = eframe::WebOptions::default();
-    eframe::start_web(
-        canvas_id,
-        web_options,
-        Box::new(|cc| Ok(Box::new(LaravelDecryptApp::new(cc)))),
-    )
-}
+// This is the entry-point for all the web-assembly.
+// This is called once from the HTML.
+// It loads the app, installs some callbacks, then returns.
+// You can add more callbacks like this if you want to call in to your code.
+// #[cfg(target_arch = "wasm32")]
+// #[wasm_bindgen]
+// pub fn start(canvas_id: &str) -> Result<(), eframe::wasm_bindgen::JsValue> {
+//     let web_options = eframe::WebOptions::default();
+//     eframe::start_web(
+//         canvas_id,
+//         web_options,
+//         Box::new(|cc| Ok(Box::new(LaravelDecryptApp::new(cc)))),
+//     )
+// }
